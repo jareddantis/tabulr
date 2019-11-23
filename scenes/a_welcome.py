400ad4c46be31032f5b2af401bbc432cfe638ff6
Jared Dantis <jareddantis@gmail.com>
2019-11-23 11:35:25 +0800

    WelcomeScreen: Fix button position, start implementing click detection

--- scenes/a_welcome.py
+++ scenes/a_welcome.py
@@ -1,19 +1,27 @@
 from ui import Button, Text
 from .scene import Scene
+from pyglet.window.mouse import *
 
 class WelcomeScreen(Scene):
-    def __init__(self, window, batch):
-        super().__init__(window, batch)
+    def __init__(self, window):
+        super().__init__(window)
         window.set_caption('Welcome to tabulr')
         self.title = Text('Welcome to', batch=self.batch,
                           x=self.margin_left, y=(self.window.height//2) + 100)
         self.title_bold = Text('tabulr', bold=True, batch=self.batch,
                                x=self.margin_left, y=(self.window.height//2) + 60)
+        self.subtitle = Text('Your schedule, from list to wallpaper. Like magic.', size=12, batch=self.batch,
+                             x=self.margin_left, y=self.window.height//2)
         self.init_sprite('next_button', Button('next', self.window, self.batch,
                                                x=self.margin_left, y=(self.window.height//2) - 100))
 
     def update(self, dt):
-        self.sprites['next_button'].update(dt)
+        pass
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == LEFT:
+            if self.is_clicked('next_button', x, y):
+                print('clicked next button')
 
     def on_mouse_motion(self, x, y, dx, dy):
         next_button = self.sprites['next_button']
