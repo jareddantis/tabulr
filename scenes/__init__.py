649f32c368dedb5e751106f00fe5853ca24c2df0
Jared Dantis <jareddantis@gmail.com>
2019-11-29 20:09:20 +0800

    util: Move Director class to separate module

--- scenes/__init__.py
+++ scenes/__init__.py
@@ -1,6 +1,5 @@
 from .a_welcome import WelcomeScreen
 from .b_course_input import CourseInputScreen
-from .director import Director
 
 # scenes/: this folder contains the definitions for each screen in the application
 #          each scene is named x_y.py, where x = letter from A to Z according to order of screen appearance,
---
+++ util/__init__.py
@@ -0,0 +1,3 @@
+from .director import Director
+
+# util/: this folder contains definitions for various reusable classes
