97c964e7d4c8ff078a3b2c1a0eae0dc6c43c238c
Jared Dantis <jareddantis@gmail.com>
2019-11-29 19:55:16 +0800

    scenes: add scene director class

---
+++ scenes/director.py
@@ -0,0 +1,31 @@
+from event_bus import EventBus
+from scenes import WelcomeScreen, CourseInputScreen
+from pyglet.window import Window
+
+class Director(EventBus):
+    def __init__(self, window: Window):
+        super().__init__()
+        self.window = window
+        self.scene = 0
+        self.scenes = [
+            WelcomeScreen(window, self),
+            CourseInputScreen(window, self)
+        ]
+
+    @property
+    def current_scene(self):
+        return self.scenes[self.scene]
+
+    def on_draw(self):
+        self.current_scene.on_draw()
+
+    def on_update(self, dt):
+        self.current_scene.update(dt)
+
+    def on_next_scene(self):
+        """
+        Change the scene that is currently being displayed to the next one.
+        """
+        if self.scene < len(self.scenes) - 1:
+            self.current_scene.on_destroy()
+            self.scene += 1
