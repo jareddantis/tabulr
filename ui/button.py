406509fb87aae335e3844b407a8f8fb64f383479
Jared Dantis <jareddantis@gmail.com>
2019-11-29 20:02:10 +0800

    button: Convert docstrings to triple quote format

--- ui/button.py
+++ ui/button.py
@@ -2,7 +2,7 @@ import pyglet
 
 class Button(pyglet.sprite.Sprite):
     def __init__(self, filename, window, batch, x=0, y=0):
-        '''
+        """
         Initialize the Button object.
         :param filename: Name of button image. For example, if we pass `tick` to this argument,
                          the application will look for the files `btn-tick.png` and `btn-tick-hover.png`
@@ -11,7 +11,7 @@ class Button(pyglet.sprite.Sprite):
         :param batch: Pyglet graphics batch. Must be the same throughout parent scene.
         :param x: x-coordinate of button position
         :param y: y-coordinate of button position
-        '''
+        """
         self.texture_default = pyglet.resource.image('btn-{}.png'.format(filename))
         self.texture_hover = pyglet.resource.image('btn-{}-hover.png'.format(filename))
         self.window = window
@@ -23,19 +23,19 @@ class Button(pyglet.sprite.Sprite):
         return image_width > x > self.x and image_height > y > self.y
 
     def on_mouse_enter(self):
-        '''
+        """
         Called when the mouse moves over the button.
         Causes the button to switch to its hover state.
-        '''
+        """
         self.image = self.texture_hover
         cursor = self.window.get_system_mouse_cursor(self.window.CURSOR_HAND)
         self.window.set_mouse_cursor(cursor)
 
     def on_mouse_leave(self):
-        '''
+        """
         Called when the mouse moves away from the button.
         Causes the button to revert to its normal state.
-        '''
+        """
         self.image = self.texture_default
         cursor = self.window.get_system_mouse_cursor(self.window.CURSOR_DEFAULT)
         self.window.set_mouse_cursor(cursor)
