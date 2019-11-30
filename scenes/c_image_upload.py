ec956d1aede2112ccd61123115e2903c1d99b77d
Jared Dantis <jareddantis@gmail.com>
2019-11-30 23:19:56 +0800

    ImageUpload: Remove Tk instance

--- scenes/c_image_upload.py
+++ scenes/c_image_upload.py
@@ -1,7 +1,7 @@
 from .scene import Scene
 from ui import Text, Button
 from pyglet.window.mouse import *
-from tkinter import Tk, filedialog
+from tkinter import filedialog
 
 class ImageUploadScreen(Scene):
     def __init__(self, window, bus):
@@ -17,18 +17,9 @@ class ImageUploadScreen(Scene):
         pick_button.y = self.window.height//2 - pick_button.height//2
         self.init_sprite('pick_button', pick_button)
 
-        # Tkinter instance for dialog
-        self.__tkinter = None
-
-    def on_pick_file(self):
-        self.__tkinter = Tk()
-        self.__tkinter.withdraw()
-        file_path = filedialog.askopenfilename(filetypes=[('Image files', '.jpg .png')])
-        print(file_path)
-        self.__tkinter.destroy()
-
     def on_mouse_press(self, x, y, button, modifiers):
         if button == LEFT:
             if self.is_clicked('pick_button', x, y):
                 # Spawn file picker
-                self.on_pick_file()
+                file_path = filedialog.askopenfilename(filetypes=[('Image files', '.jpg .png')])
+                print(file_path)
