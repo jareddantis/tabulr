ac2b974b4298e8355c283d1165b9fed48909e08f
Jared Dantis <jareddantis@gmail.com>
2019-11-30 15:28:01 +0800

    CourseInput: Switch to spawned window instead of spawning a new one

--- scenes/b2_course_view.py
+++ scenes/b2_course_view.py
@@ -1,20 +1,29 @@
 from pyglet.window import Window
-from pyglet import gl
+from pyglet import gl, graphics
 from util.scene import Scene
 from ui import Text
 
 class CourseViewer(Scene):
-    def __init__(self, bus, course_data):
+    def __init__(self, parent_window, bus, course_data):
         def on_draw():
             self.window.clear()
+            self.batch.draw()
 
-            for label in self.labels:
-                label.draw()
+        def on_close():
+            self.closed = True
+            self.window.close()
+            self.parent_window.switch_to()
+            self.parent_window.activate()
 
         # Create window
+        self.closed = False
+        self.batch = graphics.Batch()
         self.bus = bus
+        self.parent_window = parent_window
         self.window = Window(caption='View courses', style=Window.WINDOW_STYLE_TOOL)
         self.window.on_draw = on_draw
+        self.window.on_close = on_close
+        self.window.switch_to()
         gl.glClearColor(43 / 255, 65 / 255, 98 / 255, 1)
         super().__init__(self.window, bus)
 
@@ -25,6 +34,13 @@ class CourseViewer(Scene):
 
         # UI text
         self.labels = [
-            Text('View subjects', x=self.margin, y=self.window.height - self.margin, size=14),
-            Text(str(self.course_data.items()), x=self.margin, y=self.margin, size=10)
+            Text('View subjects', x=self.margin, y=self.window.height - self.margin, size=14, batch=self.batch),
+            Text(str(self.course_data.items()), x=self.margin, y=self.margin, size=10, batch=self.batch)
         ]
+
+    @property
+    def is_viewing(self):
+        return self.window.visible
+
+    def switch_to(self):
+        self.window.activate()
--- util/course_mgr.py
+++ util/course_mgr.py
@@ -1,8 +1,9 @@
 from scenes.b2_course_view import CourseViewer
 
 class CourseManager:
-    def __init__(self, bus):
+    def __init__(self, parent_window, bus):
         self.courses = {}
+        self.parent_window = parent_window
         self.viewer = None
         self.bus = bus
 
@@ -14,4 +15,12 @@ class CourseManager:
         self.courses[title] = (venue, instructor)
 
     def view_courses(self):
-        self.viewer = CourseViewer(self.bus, self.courses)
+        if self.viewer is not None and not self.viewer.closed:
+            # Window already spawned
+            if not self.viewer.is_viewing:
+                # Window is in background, so let's switch to it
+                self.viewer.switch_to()
+        else:
+            # Spawn viewer window
+            self.viewer = CourseViewer(self.parent_window, self.bus, self.courses)
+            self.viewer.switch_to()
