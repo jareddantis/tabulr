83d2303de3b7bea3e7387009f6a434d4fb378a22
Jared Dantis <jareddantis@gmail.com>
2019-12-01 19:31:08 +0800

    scenes|util: Implement bg image confirmation popup

--- scenes/c_image_upload.py
+++ scenes/c_image_upload.py
@@ -1,26 +1,76 @@
 from .scene import Scene
-from ui import Text, Button
+from ui import Text, Button, TextInput
 from pyglet.window.mouse import *
-from tkinter import filedialog
+from os import path
+from PIL import Image
+from util.image_vwr import ImageViewer
+import imghdr
 
 class ImageUploadScreen(Scene):
     def __init__(self, window, bus):
         super().__init__(window, bus, draw_waves=True, title='Select a background image')
-        self.title = Text('Select', batch=self.batch, size=22,
+        self.title = Text('Select a', batch=self.batch, size=22,
                           x=self.margin, y=self.window.height - self.margin - 22)
-        self.title_bold = Text('an image', bold=True, batch=self.batch, size=22,
+        self.title_bold = Text('background image', bold=True, batch=self.batch, size=22,
                                x=self.margin + self.title.content_width + 8, y=self.window.height - self.margin - 22)
 
+        # Instructions
+        self.subtitle = Text('Enter the path to a valid JPG or PNG image below.',
+                             x=self.margin, y=self.window.height*2//3, size=12, batch=self.batch)
+
+        # Text input
+        input_width = 400
+        self.path_input = TextInput('', self.margin, self.subtitle.y - 60, input_width, self.batch)
+        self.set_focus(self.path_input)
+
+        # Error text
+        self.error_msg.y = self.path_input.layout.y - 50
+
         # Image pick button
-        pick_button = Button('pick-image', self.window, self.batch)
-        pick_button.x = self.window.width//2 - pick_button.width//2
-        pick_button.y = self.window.height//2 - pick_button.height//2
+        pick_button = Button('pick-image', self.window, self.batch, x=self.margin, y=self.margin)
         self.init_sprite('pick_button', pick_button)
+
+        # ImageViewer
+        self.image_viewer = None
+
+    def check_file(self, file_path):
+        # Check if path is not empty
+        if len(file_path) == 0:
+            return 'Path cannot be empty'
+
+        # Check if file exists
+        if not path.isfile(file_path):
+            return 'File at specified path does not exist'
+
+        # Check if file is JPG or PNG
+        if not imghdr.what(file_path) in ['jpeg', 'png']:
+            return 'Please use a JPEG or PNG file'
+
+        # Check if file is valid
+        try:
+            test_image = Image.open(file_path)
+            test_image.verify()
+            test_image.close()
+        except:
+            return 'Image file is invalid or corrupted'
+
+        return 'ok'
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == LEFT:
             if self.sprites['pick_button'][0].hit_test(x, y):
-                # Spawn file picker
-                # Not working on macOS - we have no choice except to explicitly specify a path for now
-                file_path = filedialog.askopenfilename(filetypes=[('Image files', '.jpg .png')])
-                print(file_path)
+                # Get path
+                file_path = self.path_input.content.rstrip()
+                result = self.check_file(file_path)
+
+                if result == 'ok':
+                    self.image_viewer = ImageViewer(file_path, self.window, self.on_viewer_closed)
+                else:
+                    self.set_error_message(result)
+            elif self.path_input.hit_test(x, y):
+                self.set_focus(self.path_input)
+                self.path_input.caret.on_mouse_press(x, y, button, modifiers)
+
+    def on_viewer_closed(self, proceed):
+        if proceed:
+            self.bus.emit('next_scene')
---
+++ util/image_vwr.py
@@ -0,0 +1,64 @@
+from .popup import Popup
+from ui import Text, Button
+from pyglet.image import load
+from pyglet.sprite import Sprite
+from pyglet.graphics import OrderedGroup
+from pyglet.window import mouse
+from pyglet import gl
+
+class ImageViewer(Popup):
+    def __init__(self, file_path, parent_window, on_close):
+        super().__init__(parent_window, on_close, caption='Confirm background image',
+                         width=640, height=640)
+
+        # Layer groups
+        bg = OrderedGroup(0)
+        fg = OrderedGroup(1)
+
+        # Compute image sprite size, preserving aspect ratio
+        bg_image = load(file_path)
+        bg_image.anchor_x = bg_image.width//2
+        bg_image.anchor_y = bg_image.height//2
+        if bg_image.height > bg_image.width:
+            scale = 640 / bg_image.height
+        else:
+            scale = 640 / bg_image.width
+
+        # Background gradient
+        gl.glEnable(gl.GL_BLEND)
+        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+        self.bg_gradient = self.batch.add(4, gl.GL_QUADS, fg,
+                                          ('v2i', (0, 0, 640, 0, 640, 80, 0, 80)),
+                                          ('c4B', (0, 0, 0, 255, 0, 0, 0, 255,
+                                                   0, 0, 0, 0, 0, 0, 0, 0)))
+
+        # Draw bg image
+        self.bg_sprite = Sprite(bg_image, batch=self.batch, group=bg, x=320, y=320)
+        self.bg_sprite.scale = scale
+
+        # Confirmation text
+        self.confirm_text = Text('Is this the image you want to use?', group=fg,
+                                 size=12, x=self.margin, y=self.margin, bold=True, batch=self.batch)
+
+        # Confirmation buttons
+        self.buttons = [
+            Button('yes', self, self.batch, x=590, y=30, group=fg),
+            Button('no', self, self.batch, x=550, y=30, group=fg)
+        ]
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        for button in self.buttons:
+            if button.hit_test(x, y):
+                button.on_mouse_enter()
+                return
+            else:
+                button.on_mouse_leave()
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        if button == mouse.LEFT:
+            if self.buttons[0].hit_test(x, y):
+                # Proceed to next scene
+                self.on_close(True)
+            elif self.buttons[1].hit_test(x, y):
+                # Pick image again
+                self.on_close(False)
