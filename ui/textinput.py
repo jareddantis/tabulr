79d0c58e71e84d8e1a3719e98427ef870b8c7c20
Jared Dantis <jareddantis@gmail.com>
2019-12-01 11:40:12 +0800

    TextInput: Increase padding, change font and background color

--- ui/textinput.py
+++ ui/textinput.py
@@ -6,13 +6,16 @@ class Rectangle(object):
     def __init__(self, x1, y1, x2, y2, batch):
         self.vertex_list = batch.add(4, pyglet.gl.GL_QUADS, None,
                                      ('v2i', [x1, y1, x2, y1, x2, y2, x1, y2]),
-                                     ('c4B', [200, 200, 220, 255] * 4)
+                                     ('c4B', [245, 240, 246, 255] * 4)
                                      )
 
 class TextInput:
     def __init__(self, text, x, y, width, batch):
         self.document = pyglet.text.document.UnformattedDocument(text)
-        self.document.set_style(0, len(self.document.text), dict(color=(0, 0, 0, 255)))
+        self.document.set_style(0, len(self.document.text), {
+            'color': (0, 0, 0, 255),
+            'font_name': 'Rubik'
+        })
         font = self.document.get_font()
         height = font.ascent - font.descent
 
@@ -20,13 +23,11 @@ class TextInput:
             self.document, width, height, multiline=False, batch=batch)
         self.caret = pyglet.text.caret.Caret(self.layout)
 
-        self.layout.x = x
-        self.layout.y = y
-
-        # Rectangular outline
-        pad = 2
-        self.rectangle = Rectangle(x - pad, y - pad,
-                                   x + width + pad, y + height + pad, batch)
+        # Rectangular outline with padding
+        pad = 8
+        self.layout.x = x + pad
+        self.layout.y = y + pad
+        self.rectangle = Rectangle(x, y, x + width + pad*2, y + height + pad*2, batch)
 
     @property
     def content(self):
