a6cd251d5486188dd13bee48632bb0e26b7332a2
Jared Dantis <jareddantis@gmail.com>
2019-12-01 12:18:12 +0800

    schedule.py: Allow running directly from PyCharm

--- behind/schedule.py
+++ behind/schedule.py
@@ -259,17 +259,19 @@ for weekday in day:
         orderedclass.append('')
     schedule[weekday] = orderedclass
 
-df = pd.DataFrame(schedule)
-print(df)
 
-fig, ax = plt.subplots(figsize=(10.8, 19.2)) # set size frame
-ax.xaxis.set_visible(False)  # hide the x axis
-ax.yaxis.set_visible(False)  # hide the y axis
-ax.set_frame_on(False)  # no visible frame, uncomment if size is ok
-tabla = table(ax, df, loc='center', colWidths=[0.175]*len(df.columns))  # where df is your data frame
-tabla.auto_set_font_size(True) # Activate set fontsize manually
-# tabla.set_fontsize(12) # if ++fontsize is necessary ++colWidths
-tabla.scale(1.2, 1.2) # change size table
-plt.savefig('table.png', transparent=True)
+if __name__ == "__main__":
+    df = pd.DataFrame(schedule)
+    print(df)
 
-# Dimensions: 1080 x 1920
+    fig, ax = plt.subplots(figsize=(10.8, 19.2)) # set size frame
+    ax.xaxis.set_visible(False)  # hide the x axis
+    ax.yaxis.set_visible(False)  # hide the y axis
+    ax.set_frame_on(False)  # no visible frame, uncomment if size is ok
+    tabla = table(ax, df, loc='center', colWidths=[0.175]*len(df.columns))  # where df is your data frame
+    tabla.auto_set_font_size(True) # Activate set fontsize manually
+    # tabla.set_fontsize(12) # if ++fontsize is necessary ++colWidths
+    tabla.scale(1.2, 1.2) # change size table
+    plt.savefig('table.png', transparent=True)
+
+    # Dimensions: 1080 x 1920
