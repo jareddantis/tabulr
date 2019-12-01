bc9a7327e19905fa0558f02f4ccdce7b6a3e04b2
Jared Dantis <jareddantis@gmail.com>
2019-12-01 09:18:13 +0800

    schedule.py: Change classdict structure
    Since two courses can have the same name (lec/disc) but not the same section/timeslot, we use the section as dict key instead of the course title.

--- behind/schedule.py
+++ behind/schedule.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from pandas.plotting import table
 
-classdict = {'1 Title': ('THU', '1 BLDG Venue', '1 Instructor'),
-        '2 Title': ('WFZ', '2 BLDG Venue', '2 Instructor'),
-        '3 Title': ('DE', '3 BLDG Venue', '3 Instructor')}
+classdict = {'THU': ('1 Title', '1 BLDG Venue', '1 Instructor'),
+             'WFZ': ('2 Title', '2 BLDG Venue', '2 Instructor'),
+             'DE': ('3 Title', '3 BLDG Venue', '3 Instructor')}
 
 monlist = []
 tueslist = []
@@ -14,41 +14,41 @@ frilist = []
 satlist = []
 
 # sorting classes into days
-for subject in classdict:
+for section, course in classdict.items():
     # 1 - 1.5 hour classes
-    if classdict[subject][0][:2] == 'TH':
-        tueslist.append(subject)
-        thurslist.append(subject)
-    elif classdict[subject][0][:2] == 'WF':
-        wedlist.append(subject)
-        frilist.append(subject)
-    elif classdict[subject][0][:1] == 'M':
-        monlist.append(subject)
-    elif classdict[subject][0][:1] == 'S':
-        satlist.append(subject)
+    if section[:2] == 'TH':
+        tueslist.append(course[0])
+        thurslist.append(course[0])
+    elif section[:2] == 'WF':
+        wedlist.append(course[0])
+        frilist.append(course[0])
+    elif section[:1] == 'M':
+        monlist.append(course[0])
+    elif section[:1] == 'S':
+        satlist.append(course[0])
     # 2 hour classes
-    elif classdict[subject][0][:3] == 'ABC':
+    elif section[:3] == 'ABC':
         # monday 8-10
-        monlist.append(subject)
-    elif classdict[subject][0][:2] == 'DE':
+        monlist.append(course[0])
+    elif section[:2] == 'DE':
         # monday 10-12
-        monlist.append(subject)
+        monlist.append(course[0])
     # 3 hour classes
-    elif classdict[subject][0][:1] == 'A':
+    elif section[:1] == 'A':
         # tuesday 11:30 - 2:30
-        tueslist.append(subject)
-    elif classdict[subject][0][:1] == 'B':
+        tueslist.append(course[0])
+    elif section[:1] == 'B':
         # tuesday 8:30 - 11:30
-        tueslist.append(subject)
-    elif classdict[subject[0][:1]] == 'C':
+        tueslist.append(course[0])
+    elif section[:1] == 'C':
         # W 8:30-11:30AM lab TBA
-        wedlist.append(subject)
-    elif classdict[subject[0][:1]] == 'D':
+        wedlist.append(course[0])
+    elif section[:1] == 'D':
         # Th 8:30-11:30AM lab TBA
-        thurslist.append(subject)
-    elif classdict[subject[0][:1]] == 'E':
+        thurslist.append(course[0])
+    elif section[:1] == 'E':
         # F 8:30-11:30AM lab TBA
-        frilist.append(subject)
+        frilist.append(course[0])
 
 day = {'Mon': monlist,
        'Tues': tueslist,
@@ -70,10 +70,11 @@ classtimes = {}
 
 # creating active times
 timeslots = []
-for subject in classdict:
-    if subject[0][1:] == 'Q':
+for section, course in classdict.items():
+    subject = course[0]
+    if section[1:] == 'Q':
         # Q 7:00 - 8:30 AM
-        classtimes[subject] = ('07:00','08:30')
+        classtimes[subject] = ('07:00', '08:30')
         if '07:00' in timeslots:
             pass
         else:
@@ -82,9 +83,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('08:30')
-    elif classdict[subject][0][2:] == 'R':
+    elif section[2:] == 'R':
         # R 8:30 - 10:00 AM
-        classtimes[subject] = ('08:30','10:00')
+        classtimes[subject] = ('08:30', '10:00')
         if '08:30' in timeslots:
             pass
         else:
@@ -93,9 +94,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('10:00')
-    elif classdict[subject][0][2:] == 'U':
+    elif section[2:] == 'U':
         # U 10:00 - 11:30
-        classtimes[subject] = ('10:00','11:30')
+        classtimes[subject] = ('10:00', '11:30')
         if '10:00' in timeslots:
             pass
         else:
@@ -104,9 +105,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('11:30')
-    elif classdict[subject][0][2:] == 'V':
+    elif section[2:] == 'V':
         # V 11:30 - 1:00 PM
-        classtimes[subject] = ('11:30','13:00')
+        classtimes[subject] = ('11:30', '13:00')
         if '11:30' in timeslots:
             pass
         else:
@@ -115,9 +116,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('13:90')
-    elif classdict[subject][0][2:] == 'W':
+    elif section[2:] == 'W':
         # W 1:00 - 2:30
-        classtimes[subject] = ('13:00','14:30')
+        classtimes[subject] = ('13:00', '14:30')
         if '13:00' in timeslots:
             pass
         else:
@@ -126,9 +127,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('14:30')
-    elif classdict[subject][0][2:] == 'X':
+    elif section[2:] == 'X':
         # X 2:30 - 4:00
-        classtimes[subject] = ('14:30','16:00')
+        classtimes[subject] = ('14:30', '16:00')
         if '14:30' in timeslots:
             pass
         else:
@@ -137,9 +138,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('16:00')
-    elif classdict[subject][0][2:] == 'Y':
+    elif section[2:] == 'Y':
         # Y 4:00 - 5:30
-        classtimes[subject] = ('16:00','17:30')
+        classtimes[subject] = ('16:00', '17:30')
         if '16:00' in timeslots:
             pass
         else:
@@ -148,9 +149,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('17:30')
-    elif classdict[subject][0][2:] == 'Z':
+    elif section[2:] == 'Z':
         # Z 5:30 - 7:00
-        classtimes[subject] = ('17:30','19:00')
+        classtimes[subject] = ('17:30', '19:00')
         if '17:30' in timeslots:
             pass
         else:
@@ -159,9 +160,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('19:00')
-    elif classdict[subject][0][:3] == 'ABC':
+    elif section[:3] == 'ABC':
         # monday 8-10
-        classtimes[subject] = ('08:00','10:00')
+        classtimes[subject] = ('08:00', '10:00')
         if '08:00' in timeslots:
             pass
         else:
@@ -170,9 +171,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('10:00')
-    elif classdict[subject][0][:2] == 'DE':
+    elif section[:2] == 'DE':
         # monday 10-12
-        classtimes[subject] = ('10:00','12:00')
+        classtimes[subject] = ('10:00', '12:00')
         if '10:00' in timeslots:
             pass
         else:
@@ -182,9 +183,9 @@ for subject in classdict:
         else:
             timeslots.append('12:00')
     # 3 hour classes
-    elif classdict[subject][0][:1] == 'A':
+    elif section[:1] == 'A':
         # tuesday 11:30 - 2:30
-        classtimes[subject] = ('11:30','14:30')
+        classtimes[subject] = ('11:30', '14:30')
         if '11:30' in timeslots:
             pass
         else:
@@ -193,9 +194,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('14:30')
-    elif classdict[subject][0][:1] == 'B':
+    elif section[:1] == 'B':
         # tuesday 8:30 - 11:30
-        classtimes[subject] = ('08:30','11:30')
+        classtimes[subject] = ('08:30', '11:30')
         if '08:30' in timeslots:
             pass
         else:
@@ -204,9 +205,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('11:30')
-    elif classdict[subject][0][:1] == 'C':
+    elif section[:1] == 'C':
         # W 8:30-11:30AM lab TBA
-        classtimes[subject] = ('08:30','11:30')
+        classtimes[subject] = ('08:30', '11:30')
         if '08:30' in timeslots:
             pass
         else:
@@ -215,9 +216,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('11:30')
-    elif classdict[subject][0][:1] == 'D':
+    elif section[:1] == 'D':
         # Th 8:30-11:30AM lab TBA
-        classtimes[subject] = ('08:30','11:30')
+        classtimes[subject] = ('08:30', '11:30')
         if '08:30' in timeslots:
             pass
         else:
@@ -226,9 +227,9 @@ for subject in classdict:
             pass
         else:
             timeslots.append('11:30')
-    elif classdict[subject][0][:1] == 'E':
+    elif section[:1] == 'E':
         # F 8:30-11:30AM lab TBA
-        classtimes[subject] = ('08:30','11:30')
+        classtimes[subject] = ('08:30', '11:30')
         if '08:30' in timeslots:
             pass
         else:
@@ -244,7 +245,7 @@ for weekday in day:
     orderedclass = []
     for subject in day[weekday]:
         startctr = 0
-        for startctr in range(len(timeslots)-1):
+        for startctr in range(len(timeslots) - 1):
             if classtimes[subject][0] == timeslots[startctr]:
                 orderedclass.append(subject)
                 durationctr = startctr + 1
